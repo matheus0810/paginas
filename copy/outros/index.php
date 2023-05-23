f01b185e8ad370e53cc6178497d99df79eab480b
matheus0810 <97914922+matheus0810@users.noreply.github.com>
2023-05-23 09:31:39 -0300

    Add files via upload

---
+++ copy/outros/index.php
@@ -0,0 +1,70 @@
+
+<link rel="stylesheet" href="https://fkpartners.com/css-aai-xp">
+<div style="margin-top: 20px;">A FK Partners é a única escola que te prepara para o exame AAI da Ancord e ainda te garante uma entrevista de emprego. Todos os alunos da FK aprovados no exame AAI podem entrar em contato com a FK Partners, que nós encaminhamos o seu currículo para um escritório de Assessores de Investimento.
+Com o material e plano de estudos mais aderente do mercado, temos certeza que a aprovação está mais perto do que você imagina. E para que você se sinta ainda mais confiante, conheça nosso <a href="https://fkpartners.com/termos-e-condicoes/">seguro reprovação FK Partners*</a>.</div>
+<h3>Este curso é para mim?</h3>
+<div>Este curso é indicado para as pessoas que desejam trabalhar como assessores de investimento e profissionais de outras áreas que queiram migrar para o mercado financeiro. Recém-formados que buscam uma profissão que só cresce nos últimos anos. Profissionais com carteiras de clientes que desejam empreender na carreira. Um assessor de investimento de sucesso possui carteira de clientes com mais de R$ 500 milhões de reais e sua remuneração gira em torno desse volume de investimentos.</div>
+<div></div>
+<h3>Por que estudar com a FK Partners?</h3>
+<ul>
+ 	<li>Experiência superior em aprendizagem;</li>
+ 	<li>Parceria com os alunos. O seu crescimento é o nosso sucesso;</li>
+ 	<li>17 anos de resultados comprovados;</li>
+ 	<li>Alunos aprovados no exame, e que desejarem, terão seus currículos enviados para um escritório recrutador;</li>
+ 	<li>Todas as grandes instituições financeiras e corretoras são parceiras da FK Partners, todas certificam seus funcionários com os nossos cursos;</li>
+ 	<li>Equipe de atendimento com suporte personalizado ao aluno;</li>
+ 	<li>Equipe de professores especializados em tirar dúvidas, com mais de 5 anos de experiência em auxiliar os alunos;</li>
+ 	<li>Plataforma de ensino estilo serviços de streaming, com carregamento rápido dos tópicos;</li>
+ 	<li>Plataforma de ensino responsiva, pode ser utilizada em computadores, tablets e celulares;</li>
+ 	<li>Apostilas, slides, exercícios, resumos, simulados e todo o material necessário para sua aprovação;</li>
+ 	<li>Professores com experiência de mercado das matérias lecionadas;</li>
+ 	<li>Simulados muito aderentes aos exames.</li>
+</ul>
+
+    <div style="margin-top: 20px;" class="container" data-raofz="14">
+        <h2 data-raofz="30" style=" text-align: center; ">Nossos diferenciais falam por nós: </h2>
+        <h3 data-raofz="24">Há 19 anos, somos referência no mercado de cursos financeiros. </h3>
+        <div class="row" data-raofz="14">
+            <div class="col-sm-6" data-raofz="14">
+                <div style=" display: flex; flex-direction: column; " class="card-columns" data-raofz="14">
+                    <div style=" border-radius: 15px 0; " class="card bg-light text-dark" data-raofz="14">
+                        <div class="card-body" data-raofz="14">
+                            <h3 data-raofz="24">Mais de <span class="text-blue" data-raofz="24">200 mil clientes formados</span></h3>
+                            <p data-raofz="14">Ao longo dos anos, consolidamos experiência em treinamentos, resultando na reunião de conhecimentos absorvidos e casos diários de sucesso.</p>
+                        </div>
+                    </div>
+                    <div style=" border-radius: 15px 0; " class="card bg-light text-dark" data-raofz="14">
+                        <div class="card-body" data-raofz="14">
+                            <h3 data-raofz="24">Metodologia <span class="text-blue" data-raofz="24">exclusiva</span></h3>
+                            <p data-raofz="14">Nossos alunos têm acesso a uma plataforma de ensino única da FKPartners, intuitiva a favor do aprendizado, além de relatórios com análise de performance individual. </p>
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+                        <div class="col-sm-6" data-raofz="14">
+
+                <div style=" display: flex; flex-direction: column; " class="card-columns" data-raofz="14">
+                    <div style=" border-radius: 15px 0; " class="card bg-light text-dark" data-raofz="14">
+                        <div class="card-body" data-raofz="14">
+                            <h3 data-raofz="24">Corpo docente <span class="text-blue" data-raofz="24">qualificado</span></h3>
+                            <p data-raofz="14">Uma equipe de mais de 100 professores muito experientes; certificados, com mestrados e MBAs, e alta experiência prática nas suas áreas de atuação, garantindo que o seu colaborador aprenda o que interessa para o mercado.</p>
+                        </div>
+                    </div>
+                    <div style=" border-radius: 15px 0; " class="card bg-light text-dark" data-raofz="14">
+                        <div class="card-body" data-raofz="14">
+                            <h3 data-raofz="24">Repertório de <span class="text-blue" data-raofz="24">sucessos</span></h3>
+                            <p data-raofz="14">O ecossistema financeiro inteiro conhece a nossa postura de inovação e qualidade. Coloque os seus colaboradores em contato com os melhores professores e conteúdos.</p>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+    
+
+        </div>
+
+    </div>
